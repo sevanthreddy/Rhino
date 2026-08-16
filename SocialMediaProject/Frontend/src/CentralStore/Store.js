573526ca9df91297e3036662efc41dd3e0d4d409
Sevanth <sevanthreddy.b@gmail.com>
2026-08-16 19:10:56 +0530

    changes to import stats

--- SocialMediaProject/Frontend/src/CentralStore/Store.js
+++ SocialMediaProject/Frontend/src/CentralStore/Store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../slices/AuthSlice";
+import authReducer from "../Slices/AuthSlice";
 
 
 export const store = configureStore({
